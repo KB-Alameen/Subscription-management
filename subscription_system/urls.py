1c32f6afee41816a24c271b8c5d28b703308b607
KB-Alameen <kareem.b@alameensoft.com>
2024-10-25 20:36:18 +0400

    Update urls.py

--- subscription_system/urls.py
+++ subscription_system/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 urlpatterns = [
+    path('', include('your_app_name.urls')),  # Replace `your_app_name` with the name of your app folder
     path('admin/', admin.site.urls),
-    path('api/',include("subscriptions.urls") ),
-    
 ]
